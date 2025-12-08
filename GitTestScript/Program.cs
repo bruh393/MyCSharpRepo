f76594379191015804078661bebb76bc9bb044fc
bruh393 <shashwatmallick335@gmail.com>
2025-12-09 00:10:28 +0530

    Update Program.cs

--- GitTestScript/Program.cs
+++ GitTestScript/Program.cs
@@ -1,7 +1,50 @@
-﻿class gitTest
+﻿//class gitTest
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("meth");
+//    }
+//}
+
+class Calc
 {
     public static void Main()
     {
-        Console.WriteLine("meth");
+        Console.Write("first digit :");
+        double n1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("second digit :");
+        double n2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("select operetor :");
+        string o = Console.ReadLine();
+        double ans = 0; 
+
+        if (o == "+")
+        {
+            ans = n1 + n2;
+        }
+        else if (o == "-")
+        {
+            ans = n1 - n2;
+        }
+        else if (o == "/")
+        {
+            ans = n1 / n2;
+        }
+        else if(o == "*")
+        {
+            ans = n1 * n2;
+        }
+        else if(o == "%")
+        {
+            ans = n1 % n2;
+        }
+        else
+        {
+            Console.WriteLine("math error");
+        }
+        
+
+        
+        Console.WriteLine(ans);
     }
 }
