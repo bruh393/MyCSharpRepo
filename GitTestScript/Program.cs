6388b03c42a5f0903e54af6cfd241bc6954d60c5
bruh393 <shashwatmallick335@gmail.com>
2025-12-10 00:01:17 +0530

    Update Program.cs

--- GitTestScript/Program.cs
+++ GitTestScript/Program.cs
@@ -1,4 +1,5 @@
-﻿//class gitTest
+﻿using System;
+//class gitTest
 //{
 //    public static void Main()
 //    {
@@ -6,45 +7,63 @@
 //    }
 //}
 
-class Calc
+//class Calc
+//{
+//    public static void Main()
+//    {
+//        Console.Write("first digit :");
+//        double n1 = Convert.ToDouble(Console.ReadLine());
+//        Console.Write("second digit :");
+//        double n2 = Convert.ToDouble(Console.ReadLine());
+//        Console.Write("select operetor :");
+//        string o = Console.ReadLine();
+//        double ans = 0; 
+
+//        if (o == "+")
+//        {
+//            ans = n1 + n2;
+//        }
+//        else if (o == "-")
+//        {
+//            ans = n1 - n2;
+//        }
+//        else if (o == "/")
+//        {
+//            ans = n1 / n2;
+//        }
+//        else if(o == "*")
+//        {
+//            ans = n1 * n2;
+//        }
+//        else if(o == "%")
+//        {
+//            ans = n1 % n2;
+//        }
+//        else
+//        {
+//            Console.WriteLine("Math Error");
+//        }
+        
+
+        
+//        Console.WriteLine(ans);
+//    }
+//}
+
+class Odd
 {
     public static void Main()
     {
-        Console.Write("first digit :");
-        double n1 = Convert.ToDouble(Console.ReadLine());
-        Console.Write("second digit :");
-        double n2 = Convert.ToDouble(Console.ReadLine());
-        Console.Write("select operetor :");
-        string o = Console.ReadLine();
-        double ans = 0; 
+        Console.Write("enter your number");
+        double n = Convert.ToDouble(Console.ReadLine());
 
-        if (o == "+")
+        if(n % 2 == 0)
         {
-            ans = n1 + n2;
-        }
-        else if (o == "-")
-        {
-            ans = n1 - n2;
-        }
-        else if (o == "/")
-        {
-            ans = n1 / n2;
-        }
-        else if(o == "*")
-        {
-            ans = n1 * n2;
-        }
-        else if(o == "%")
-        {
-            ans = n1 % n2;
+            Console.WriteLine("Even");
         }
         else
         {
-            Console.WriteLine("Math Error");
+            Console.WriteLine("Odd");
         }
-        
-
-        
-        Console.WriteLine(ans);
     }
 }
