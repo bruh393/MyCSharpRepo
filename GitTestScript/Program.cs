20e4accc3c5d85b14a392e04fad51b4ab253d064
bruh393 <shashwatmallick335@gmail.com>
2025-12-10 00:35:51 +0530

    Update Program.cs

--- GitTestScript/Program.cs
+++ GitTestScript/Program.cs
@@ -43,27 +43,70 @@
 //        {
 //            Console.WriteLine("Math Error");
 //        }
-        
 
-        
+
+
 //        Console.WriteLine(ans);
 //    }
 //}
 
-class Odd
-{
-    public static void Main()
-    {
-        Console.Write("enter your number");
-        double n = Convert.ToDouble(Console.ReadLine());
+//class Odd
+//{
+//    public static void Main()
+//    {
+//        Console.Write("enter your number");
+//        double n = Convert.ToDouble(Console.ReadLine());
 
-        if(n % 2 == 0)
-        {
-            Console.WriteLine("Even");
-        }
-        else
-        {
-            Console.WriteLine("Odd");
-        }
+//        if(n % 2 == 0)
+//        {
+//            Console.WriteLine("Even");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Odd");
+//        }
+//    }
+//}
+//class Constructor
+//{
+//    public string name;
+//    public int hp;
+//    public static void Main()
+//    {
+//        Constructor c = new Constructor();
+//        c.Player("meth", 3);
+
+
+//    }
+//    public void Player(string a, int b)
+//    {
+//        name = a;
+//        hp = b;
+//    }
+//}
+
+class Student
+{
+    public string name;
+    public int age;
+
+    public Student(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, I'm {name}, and I'm {age} years old.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s1 = new Student("Shashwat", 16);
+        s1.Introduce();
     }
 }
